84a6451a8b82c514742e958827da1170637ba18c
Thomricco <tommasoricco@hotmail.it>
2022-02-23 17:21:54 +0100

    fermo su group

--- quary_select.sql
+++ quary_select.sql
@@ -64,3 +64,89 @@ SELECT * COUNT(`id`) FROM `departments`;
 SELECT COUNT(*)
 FROM `teachers`
 WHERE `phone` IS NULL;
+
+
+/*Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia*/
+SELECT `students`.`id`,`students`.`name`,`students`.`surname`,`degrees`.name
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degree_id`
+WHERE `degrees`.`name` LIKE "%Corso di Laurea in Economia" ;
+
+
+
+/*Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze*/
+SELECT `degrees`.`id`,`degrees`.`name`,`degrees`.`level`,
+		`departments`.`name`,`departments`.`address`
+FROM `degrees`
+JOIN `departments`ON `degrees`.`department_id` = `department_id`
+WHERE `departments`.`name` LIKE "% Neuroscienze"
+
+/*Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)*/
+SELECT `teachers`.`name`,`teachers`.`surname`,`degrees`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ ON `teachers`.`id` = `course_teacher`.`course_id`
+JOIN `courses`
+	ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+	ON `courses`.`degree_id` = `degrees`.`id`
+    
+WHERE `teachers`.`name` = "Fulvio"
+	AND `teachers`.`surname`= "Amato"
+
+/*Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome*/
+
+
+SELECT `students`.`name`,`students`.`surname`,
+		`degrees`.`name`,`departments`.`name`
+FROM `students`
+JOIN `degrees`
+	ON `students`.`degree_id`= `degrees`.`id`
+JOIN `departments`
+	ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`name`, `students`.`surname`;
+
+/*Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti*/
+
+SELECT `teachers`.`name`,`teachers`.`surname`,`degrees`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ ON `teachers`.`id` = `course_teacher`.`course_id`
+JOIN `courses`
+	ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+	ON `courses`.`degree_id` = `degrees`.`id`
+
+
+
+/*Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)*/
+SELECT `teachers`.`name`,`teachers`.`surname`,`degrees`.`name`,`departments`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ ON `teachers`.`id` = `course_teacher`.`course_id`
+JOIN `courses`
+	ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+	ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+	ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` LIKE "m %"
+
+
+/*Contare quanti corsi di laurea ci sono per ogni dipartimento*/
+SELECT COUNT(`degrees`.`address`),`departments`.`name`
+FROM `degrees`
+JOIN `departments`
+	ON `degrees`.`department_id` = `departments`.`id`
+GROUP BY `departments`.`id`
+
+/*Calcolare la media dei voti di ogni appello d'esame*/
+SELECT `exams`.`id`, `exam_student`.`vote`
+FROM `exam_student`
+JOIN `exams` 
+	ON `exam_student`.`exam_id`=`exams`.`id`
+JOIN `courses`
+	ON `exams`.`course_id`=`courses`.`id`
+JOIN `degrees`
+	ON `courses`.`degree_id`=`degrees`.`id`
+GROUP BY `exams`.`id`
